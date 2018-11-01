e499476ede3527353b340e26c22019d45046bc6a
coolbhy <945927732@qq.com>
2018-11-01 22:57:09 +0800

    1.设置CrimeFragment的布局 2.将fragment_crime.xml绑定至CrimeFragment，设置监听函数，确定逻辑。 3.将fragment放置在activity_crime中。

--- app/src/main/java/com/example/bin/crinimalitent/CrimeFragmen.java
+++ app/src/main/java/com/example/bin/crinimalitent/CrimeFragmen.java
@@ -1,11 +1,77 @@
 package com.example.bin.crinimalitent;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import static android.widget.CompoundButton.*;
 
 /**
  * @Author: Bhy
  * @Date: 2018/11/1
  */
 public class CrimeFragmen extends Fragment {
-    
+    private Crime mCrime;
+    private EditText mTitleFiled;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_crime,container,false);
+
+        mTitleFiled = v.findViewById(R.id.crime_title);
+        mTitleFiled.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //通过监听用户输入来设置Crime的title
+               mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mDateButton = v.findViewById(R.id.crime_date);
+        //显示日期
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            //为保持代码整洁，alt+Enter，选中Add on demand static import for 'android.widget.CompoundButton'， 去除前面的包名
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //当checkBox状态改变时，同时改变对应Crime的相应状态
+                mCrime.setSovled(isChecked);
+            }
+        });
+
+        return v;
+    }
 }
