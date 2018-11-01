4ad5040cd8c35ac2dc2d06d51f33591291ca862d
coolbhy <945927732@qq.com>
2018-11-01 22:14:46 +0800

    learning criminalIntent

---
+++ app/src/main/java/com/example/bin/crinimalitent/CrimeFragmen.java
@@ -0,0 +1,11 @@
+package com.example.bin.crinimalitent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @Author: Bhy
+ * @Date: 2018/11/1
+ */
+public class CrimeFragmen extends Fragment {
+    
+}
